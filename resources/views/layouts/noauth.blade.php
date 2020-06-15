5d17b5e512ca1f1c2300159c81e815d70ea13732
Ayodeji Odukoya <ayodeji.od@gmail.com>
2020-06-15 20:16:30 +0100

    filter bar

--- resources/views/layouts/noauth.blade.php
+++ resources/views/layouts/noauth.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Rick and Morty Characters
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -65,6 +65,7 @@
                             <option value="unknown">unknown</option>
                         </select>
                         <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                        <a href="{{route("characters.index")}}" class="btn btn-danger ml-2 mb-2">Clear Filter</a>
                     </form>
                 </div>
             </div>
